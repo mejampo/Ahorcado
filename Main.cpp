f9267f12d02c3110dfe77b66a321d02e8e4b00cb
MJ <mejampo@unitec.edu>
2015-02-22 18:42:56 -0600

    update

--- Main.cpp
+++ Main.cpp
@@ -2,25 +2,36 @@
 #include <ncurses.h>
 #include <cstdlib>
 #include <ctime>
+#include <string>
 
 //PARA COMPILAR NCURSES g++ Main.cpp -lncurses -o main
 using namespace std;
 
 int main(int argc, char** argv){
 
-
 	int dificultad;
 	bool gano;
 	int opc;
+	int ch;
 
 	//printw(15,50,"BIENVENID@");
 
 	initscr();			/* Start curses mode 		  */
-	printw("Hello Player!!!\n");	/* Print Hello World		  */
-	refresh();			/* Print it on to the real screen */
-	printw("Welcome to your ruin");
+	raw();				
+	keypad(stdscr, TRUE);		
+	noecho();
+	printw("Hello Player!!!\n");
+
 	refresh();
-	getch();			/* Wait for user input */
+	getch();
+
+	printw("The pressed key is ");
+		attron(A_BOLD);
+		printw("%c", ch);
+		attroff(A_BOLD);
+
+		refresh();	
+    	getch();			
 	endwin();
 	/*do{
 
@@ -28,3 +39,4 @@ int main(int argc, char** argv){
 
 	return 0;
 }
+
