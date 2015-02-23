a2392395a6a23e0b00ad184cce29466074111e31
MJ <mejampo@unitec.edu>
2015-02-23 00:16:08 -0600

    todo

--- Main.cpp
+++ Main.cpp
@@ -3,137 +3,250 @@
 #include <cstdlib>
 #include <ctime>
 #include <string>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <iomanip>
 
-//PARA COMPILAR NCURSES g++ Main.cpp -lncurses -o main
 using namespace std;
-
-#define WIDTH 30
-#define HEIGHT 10 
-
-int startx = 0;
-int starty = 0;
-
-char *choices[] = { "FACIL","MEDIO","DIFICIL","Exit"};
-
-int n_choices = sizeof(choices) / sizeof(char *);
-void print_menu(WINDOW *menu_win, int highlight);
-
-
 int main(int argc, char** argv){
 
-	int dificultad;
 	bool gano;
-	int opc;
-	int ch;
+	int resp;
+	string meter= "";
+	vector<string> facil;
+	vector<string> medio;
+	vector<string> dificil;
+	int ganado=0;
+	int perdido=0;
+	vector<string> hangman;
+	hangman.push_back("_______ ");
+	hangman.push_back("|      | ");
+	hangman.push_back("|      | ");
+	hangman.push_back("|     ___ ");
+	hangman.push_back("|    |O_O| ");
+	hangman.push_back("|     --- ");
+	hangman.push_back("|      | ");
+	hangman.push_back("|   ---|--- ");
+	hangman.push_back("|   |  |   |");
+	hangman.push_back("|   |  |   |");
+	hangman.push_back("|      |    ");
+	hangman.push_back("|     ---- ");
+	hangman.push_back("|    |    | ");
+	hangman.push_back("|    |    | ");
+	hangman.push_back("|   _|    |_ ");
+	hangman.push_back("|__________________");
 
- 	WINDOW *menu_win;
-	int highlight = 1;
-	int choice = 0;
-	int c;
+	//LLENA VECTORS CON LAS PALABRAS
+   		ifstream fe("FACIL.txt"); 
+	   while(!fe.eof()) {
+	   	fe >> meter;
+   		facil.push_back(meter);
+ 	 	 }
+  		 fe.close();
 
-	initscr();
-	clear();
-	noecho();
-	cbreak();	/* Line buffering disabled. pass on everything */
-	startx = (80 - WIDTH) / 2;
-	starty = (24 - HEIGHT) / 2;
+  		 ifstream fe2("MEDIO.txt"); 
+	   while(!fe2.eof()) {
+	   	fe2 >> meter;
+   		medio.push_back(meter);
+ 	 	 }
+  		 fe.close();
+
+  		 ifstream fe3("DIFICIL.txt"); 
+	   while(!fe3.eof()) {
+	   	fe3 >> meter;
+   		dificil.push_back(meter);
+ 	 	 }
+  		 fe.close();
+  	// TERMINA DE ABRIR ARCHIVOS
+
+
+	do{
+		int difi;
+		cout<<"*********JUEGO DEL AHORCADO*********"<<endl;
+		cout<<"-----EN QUE DIFICULTAD DESEA JUGAR?-----\n1.- FACIL\n2.-MEDIO\n3.-DIFICIL"<<endl;
+		cin>>difi;
+
+		while(difi!=1&&difi!=2&&difi!=3){
+			cout<<"OPCION ERRONEA!!! INTENTE DE NUEVO\n-----EN QUE DIFICULTAD DESEA JUGAR?-----\n1.- FACIL\n2.-MEDIO\n3.-DIFICIL"<<endl;
+			cin>>difi;
+		}
+
+		if(difi==1){
+			int x=0;
+			string word;
+			string guess;
+			int intentos;
+			int errors;
+			srand (time(0));
+			x= rand()%facil.size()-1;
+			word = facil[x];
+			intentos= word.length();
+			errors=intentos;
+			vector<string> espacios;
+			cout<<"NIVEL FACIL"<<endl;
+
+			for(int i=0;i<word.length();i++){
+				espacios.push_back("_");
+			}
+			do{
+			
+			cout<<"TIENE "<<intentos<<" INTENTOS"<<endl;
+			cin>>guess;
+
+			if(guess.length()==1){
+				for(int i=0;i<=word.length();i++){
+					if(word[i]==guess[0]){
+						cout<<"HA ENCONTRADO UNA LETRA"<<endl;
+						espacios[i]=guess;
+					}
+				}
+			}else if(guess.length()>=1){
+				if(word==guess){
+					gano=true;
+					break;
+				}
+			}else{
+				cout<<"ESA LETRA NO SE ENCUENTRA EN LA PALABRA"<<endl;
+				errors--;
+			}
+				
+				if(intentos!=word.length()){
+					int lineas = 15/errors;
+						for(int i=0;i<=lineas;i++){
+								cout<<hangman[i]<<endl;
+						}
+					}	
+					cout<<endl;
+					for(int l=0;l<=espacios.size();l++){
+							cout<< espacios[l];
+					}
+					cout<<endl;
+				intentos--;
+			}while(gano==false&&intentos!=0);
 		
-	menu_win = newwin(HEIGHT, WIDTH, starty, startx);
-	keypad(menu_win, TRUE);
-	mvprintw(0, 0, "USE LAS FLECHAS PARA SELECCIONAR, PRESIONE ENTER PARA ELEGIR");
-	refresh();
-	print_menu(menu_win, highlight);
-	while(1)
-	{	c = wgetch(menu_win);
-		switch(c)
-		{	case KEY_UP:
-				if(highlight == 1)
-					highlight = n_choices;
-				else
-					--highlight;
-				break;
-			case KEY_DOWN:
-				if(highlight == n_choices)
-					highlight = 1;
-				else 
-					++highlight;
-				break;
-			case 10:
-				choice = highlight;
-				break;
-			default:
+		}else if(difi==2){
 
-				break;
+
+			int x=0;
+			string word;
+			string guess;
+			int intentos;
+			int errors;
+			srand (time(0));
+			x= rand()%medio.size()-1;
+			word = medio[x];
+			intentos= word.length();
+			errors=intentos;
+			vector<string> espacios;
+			cout<<"NIVEL MEDIO"<<endl;
+
+			for(int i=0;i<word.length();i++){
+				espacios.push_back("_");
+			}
+			do{
+			
+			cout<<"TIENE "<<intentos<<" INTENTOS"<<endl;
+			cin>>guess;
+
+			if(guess.length()==1){
+				for(int i=0;i<=word.length();i++){
+					if(word[i]==guess[0]){
+						cout<<"HA ENCONTRADO UNA LETRA"<<endl;
+						espacios[i]=guess;
+					}
+				}
+			}else if(guess.length()>=1){
+				if(word==guess){
+					gano=true;
+					break;
+				}
+			}else{
+				cout<<"ESA LETRA NO SE ENCUENTRA EN LA PALABRA"<<endl;
+				errors--;
+			}
+				
+				if(intentos!=word.length()){
+						int lineas = 15/errors;
+						for(int i=0;i<=lineas;i++){
+								cout<<hangman[i]<<endl;
+						}
+					}	
+					cout<<endl;
+					for(int l=0;l<=espacios.size();l++){
+							cout<< espacios[l];
+					}
+					cout<<endl;
+				intentos--;
+			}while(gano==false&&intentos!=0);
+
+		}else if(difi==3){
+
+			int x=0;
+			string word;
+			string guess;
+			int intentos;
+			int errors;
+			srand (time(0));
+			x= rand()%dificil.size()-1;
+			word = dificil[x];
+			intentos= word.length();
+			errors=intentos;
+			vector<string> espacios;
+			cout<<"NIVEL FACIL"<<endl;
+
+			for(int i=0;i<word.length();i++){
+				espacios.push_back("_");
+			}
+			do{
+			
+			cout<<"TIENE "<<intentos<<" INTENTOS"<<endl;
+			cin>>guess;
+
+			if(guess.length()==1){
+				for(int i=0;i<=word.length();i++){
+					if(word[i]==guess[0]){
+						cout<<"HA ENCONTRADO UNA LETRA"<<endl;
+						espacios[i]=guess;
+					}
+				}
+			}else if(guess.length()>=1){
+				if(word==guess){
+					gano=true;
+					break;
+				}
+			}else{
+				cout<<"ESA LETRA NO SE ENCUENTRA EN LA PALABRA"<<endl;
+				errors--;
+			}
+				
+				if(intentos!=word.length()){
+
+						int lineas = 15/errors;
+						for(int i=0;i<=lineas;i++){
+								cout<<hangman[i]<<endl;
+						}
+					}	
+					cout<<endl;
+					for(int l=0;l<=espacios.size();l++){
+							cout<< espacios[l];
+					}
+					cout<<endl;
+				intentos--;
+			}while(gano==false&&intentos!=0);
+
 		}
 
-		print_menu(menu_win, highlight);
-		if(choice != 0)	/* User did a choice come out of the infinite loop */
-			break;
-	}	
-	clrtoeol();
-	refresh();
+				if(gano==true){
+					cout<<"FELICIDADES HA GANADO ESTA RONDA"<<endl;
+					ganado++;
+				}else{
+					cout<<"PERDIO ESTA VEZ"<<endl;
+					perdido++;
+				}
+		cout<<"Desea jugar de nuevo? \nSI=1\nNO=0"<<endl;
+		cin>>resp;
+	}while (resp!=0);
 
-		if(choice == 1){	//FACIL
-			char ans[80];
-			initscr();
-			noecho();
-			bool gano= false;
-			int intentos=0;
-			printw("MODO FACIL");
-				do{
-
-					mvprintw(LINES-2,0,"Lleva ",intentos," intentos");
-
-				}while(gano==false);
-
-			/*getstr(ans);
-
- 			mvprintw(LINES - 2, 0, "You Entered: %s", ans);*/
-
- 			getch();
- 			endwin();
-
-		}else if(choice==2){//MEDIO
-			char ans[80];
-			initscr();
-			noecho();
-			printw("MODO MEDIO");
-
-			getch();
- 			endwin();
-
-		}else if(choice ==3){//DIFICIL
-			char ans[80];
-			initscr();
-			noecho();
-			printw("MODO DIFICIL");
-
-			getch();
- 			endwin();
-
-		}
-	endwin();
 	return 0;
 }
-
-
-void print_menu(WINDOW *menu_win, int highlight)
-{
-	int x, y, i;	
-
-	x = 2;
-	y = 2;
-	box(menu_win, 0, 0);
-	for(i = 0; i < n_choices; ++i)
-	{	if(highlight == i + 1) /* High light the present choice */
-		{	wattron(menu_win, A_REVERSE); 
-			mvwprintw(menu_win, y, x, "%s", choices[i]);
-			wattroff(menu_win, A_REVERSE);
-		}
-		else
-			mvwprintw(menu_win, y, x, "%s", choices[i]);
-		++y;
-	}
-	wrefresh(menu_win);
-}
-
